1e6bb0c0de43760f00d60797b1debaafc281b0e9
simonf2000 <simonf2000@outlook.com>
2024-05-14 20:19:59 +0800

    change deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -7,7 +7,7 @@ set -e
 npm run docs:build
 
 # 进入生成的文件夹
-cd E:\Java\Java-Learning-Resource\VuePress-markdown-master\docs\.vuepress\dist
+cd "E:/Java/Java-Learning-Resource/VuePress-markdown-master/docs/.vuepress/dist"
 
 # 如果是发布到自定义域名
 # echo 'www.example.com' > CNAME
